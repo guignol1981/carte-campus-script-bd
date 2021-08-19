c68aa193b8ef7a482a2f93bdfa3a3116fd61f949
Manuel Grenier <manuel.grenier@dti.ulaval.ca>
2021-08-19 14:32:51 -0400

    Retrait des attributs, collection, etc hardcodé. Ajout des 3 environements/paliers Filtre et transformation des GeoPoints par regex Importation de plusieurs collections dans un fichier source

--- app.ts
+++ app.ts
@@ -1,41 +1,42 @@
 import firebase from 'firebase';
 import data from './data.json';
+import projectConf from './fs_projects_conf.json';
 
-const firebaseConfig = {
-    apiKey: 'AIzaSyBLndwQkXf9rqkhZ7-xUsLsFLXyEjtzWpY',
-    authDomain: 'monportail-test.firebaseapp.com',
-    databaseURL: 'https://monportail-test.firebaseio.com',
-    projectId: 'monportail-test',
-    storageBucket: 'monportail-test.appspot.com',
-    messagingSenderId: '126796368333',
-    appId: '1:126796368333:web:1c9ed7d646425755d21c84',
-    measurementId: 'G-9X57FKCVLQ',
-};
 
-const firebaseApp = firebase.initializeApp(firebaseConfig);
+const geoPointRegex = /(-?\d{2}\.\d{1,}),(-?\d{2}\.\d{1,})/;
+  
+const firebaseApp = firebase.initializeApp(projectConf.monportailTest);
 const firestore = firebaseApp.firestore();
 
-const COLLECTION = 'places';
 
-Object.keys(data.places).forEach((id) => {
-    const raw = (data.places as any)[id];
-    const place = { ...raw };
 
-    Object.keys(place).forEach((k) => {
-        if (place[k] === '') delete place[k];
-        if (Array.isArray(place[k])) delete place[k];
-    });
-
-    place.markerCoordinates = new firebase.firestore.GeoPoint(
-        raw.markerCoordinates.split(',')[0],
-        raw.markerCoordinates.split(',')[1]
-    );
-
-    if (raw.geometry) {
-        place.geometry = raw.geometry.map(
-            (g: any) => new firebase.firestore.GeoPoint(g[0], g[1])
-        );
-    }
-
-    firestore.collection(COLLECTION).doc(id).set(place);
+Object.keys(data).forEach( collectionId => {			//Traitement des collections
+	let collection: any = (data as any)[collectionId];
+	Object.keys(collection).forEach( documentId => {	//Traitement des documents
+		let doc: any = collection[documentId];
+		Object.keys(doc).forEach( key => {				//Traitement des attributs
+			 doc[key] = transformType(doc[key]);
+		});
+		
+		//Push le document dans Firestore
+		firestore.collection(collectionId).doc(documentId).set(doc,{merge: true});
+	});
 });
+
+
+
+function transformType( attribute : any ){
+	if (Array.isArray(attribute)){ //Recursion pour tranformer le contenu des array
+		attribute.forEach((arrayElem, arrIndex) => {
+			attribute[arrIndex] = transformType(arrayElem);
+		});
+	}else{ 
+		//Transformation des coordonées en GeoPoints
+		let match;
+		if( match = attribute.match(geoPointRegex) ){
+			attribute = new firebase.firestore.GeoPoint(match[1], match[2]);
+		}
+	}
+	return attribute;
+}
+
